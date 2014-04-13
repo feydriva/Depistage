a1a041a27af4b7c2d8c51c5bf1fbabf184c00ca1
LePelicaN <lepelican@gmail.com>
2014-04-13 23:53:51 +0200

    Ajout du remplacement lors de l'execution.

--- src/main/ProgrammeEnregistrement.cpp
+++ src/main/ProgrammeEnregistrement.cpp
@@ -106,7 +106,9 @@ void lancerGenerationWord( const QString & cheminWord, const QString & cheminDos
                            const QString & codePatientAvant, const QString & codePatientApres,
                            const QString & sexAvant, const QString & sexApres,
                            const QString & dateNaissanceAvant, const QString & dateNaissanceApres,
-                           const QString & dateDuJourAvant, const QString & dateDuJourApres )
+                           const QString & dateDuJourAvant, const QString & dateDuJourApres,
+                           const QString & realiseParAvant, const QString & realiseParApres,
+                           const QString & renduParAvant, const QString & renduParApres )
 {
    depistage::office::Word word;
    depistage::office::WordDocument document = word.open( cheminWord.toStdString() );
@@ -114,6 +116,8 @@ void lancerGenerationWord( const QString & cheminWord, const QString & cheminDos
    document.replace( sexAvant.toStdString(), sexApres.toStdString() );
    document.replace( dateNaissanceAvant.toStdString(), dateNaissanceApres.toStdString() );
    document.replace( dateDuJourAvant.toStdString(), dateDuJourApres.toStdString() );
+   document.replace( realiseParAvant.toStdString(), realiseParApres.toStdString() );
+   document.replace( renduParAvant.toStdString(), renduParApres.toStdString() );
    unsigned point = cheminWord.toStdString().find_last_of( "." );
    const std::string extension = cheminWord.toStdString().substr( point );
    std::string nouveauFichier =
@@ -150,14 +154,18 @@ void ProgrammeEnregistrement::lancerGeneration()
         m_fichierExcel->getChemin() != "" &&
         m_codePatient->motARemplacer() != "" &&
         m_sex->motARemplacer() != "" &&
-        m_dateDeNaissance->motARemplacer() != "" )
+        m_dateDeNaissance->motARemplacer() != "" &&
+        m_realisePar->motARemplacer() != "" &&
+        m_renduPar->motARemplacer() != "")
    {
       lancerGenerationWord(
          m_fichierWord->getChemin(), m_dossierWord->getChemin(),
          m_codePatient->motARemplacer(), m_codePatient->motParLequelRemplacer(),
          m_sex->motARemplacer(), m_sex->motParLequelRemplacer(),
          m_dateDeNaissance->motARemplacer(), m_dateDeNaissance->motParLequelRemplacer(),
-         m_dateDuJour->motARemplacer(), m_dateDuJour->motParLequelRemplacer() );
+         m_dateDuJour->motARemplacer(), m_dateDuJour->motParLequelRemplacer(),
+         m_realisePar->motARemplacer( ), m_realisePar->motParLequelRemplacer( ),
+         m_renduPar->motARemplacer( ), m_renduPar->motParLequelRemplacer( ) );
       lancerGenerationExcel(
          m_fichierExcel->getChemin(),
          m_codePatient->motParLequelRemplacer(),
@@ -178,6 +186,10 @@ void ProgrammeEnregistrement::sauverConfig()
       config::RemplacerConfig( labelDateDeNaissance, m_dateDeNaissance->motARemplacer() ) );
    getConfig().miseAJourRemplacerConfig(
       config::RemplacerConfig( labelDateDuJour, m_dateDuJour->motARemplacer() ) );
+   getConfig().miseAJourRemplacerConfig(
+      config::RemplacerConfig( labelRealisePar, m_realisePar->motARemplacer() ) );
+   getConfig().miseAJourRemplacerConfig(
+      config::RemplacerConfig( labelRenduPar, m_renduPar->motARemplacer() ) );
    getConfig().miseAJourSelectionFichierConfig(
       config::SelectionFichierConfig( labelWord, m_fichierWord->getChemin() ) );
    getConfig().miseAJourSelectionFichierConfig(
